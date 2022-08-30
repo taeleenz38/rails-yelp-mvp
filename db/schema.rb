f1617c390cf813140f0968d36829c04657d91dba
Tae Lee <taekyu.lee3@gmail.com>
2022-08-30 15:07:25 +1000

    advanced CRUD - restaurants/reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_014747) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_022801) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "address"
@@ -25,6 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_014747) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,19 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dragon_hotpot = {name: "Mcdonalds", address: "China", category: "chinese"}
+woodfire_pizza = { name: "Woodfire Pizza", address: "Milan", category: "italian" }
+toast = {name: "Toast", address: "Paris", category: "french"}
+sushi = {name: "Sushi", address: "Hokkaido", category: "japanese"}
+beer = {name: "Beer", address: "Chocolate", category: "belgian"}
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+[dragon_hotpot, woodfire_pizza, toast, sushi, beer].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
